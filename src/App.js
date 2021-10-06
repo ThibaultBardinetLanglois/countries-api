954eb3e2fae71cdc1ed0a33f9237d413ca57f586
thib <langlois.bardinet.thibault@gmail.com>
2021-10-06 14:00:09 +0200

    try to link pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CountryDetails from "./pages/CountryDetails";
-import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/country/:country" exact component={CountryDetails} />
-        <Route component={NotFound} />
+        <Route path="/" component={Home} />
+        <Route path="/country/:country"  component={CountryDetails} />
       </Switch>
     </BrowserRouter>
   );
